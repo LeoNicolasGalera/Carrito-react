45f5de8c58faa5898dc44ba600bb28d019d8416f
LeoNicolasGalera <109870148+LeoNicolasGalera@users.noreply.github.com>
2023-07-28 21:04:26 -0300

    mas cambios

--- src/componentes/Tarjetas/Tarjetas.jsx
+++ src/componentes/Tarjetas/Tarjetas.jsx
@@ -4,18 +4,16 @@ function Tarjeta(props) {
   const { id, title, img, price } = props;
 
   return (
-    <>
-      <div>
-        <h1>{title}</h1>
-        <img src={img} alt="" width={"50%"} />
-        <p>
-          <h3>${price}</h3>
-        </p>
-        <Link to={`/productos/${id}`}>
-          <button style={{ fontSize: "16px" }}>Cotizar</button>
-        </Link>
-      </div>
-    </>
+    <div>
+      <h1>{title}</h1>
+      <img src={img} alt="" width={"50%"} />
+      <p>
+        <h3>${price}</h3>
+      </p>
+      <Link to={`/productos/${id}`}>
+        <button style={{ fontSize: "16px" }}>Cotizar</button>
+      </Link>
+    </div>
   );
 }
 
